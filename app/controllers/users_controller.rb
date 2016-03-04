c17289dc8e90bdf37c33553a9654c1d08af41a90
Steven King <stevenking86@gmail.com>
2016-03-04 15:10:29 -0500

    Get user registration working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to '/'
+    else
+      render :new
+    end
   end
 
 
 
+  private
 
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
+  end
 
 
 end

56b43c0b0029b92c07e0a51bea2ccb0804aa4d33
Devin Mandelbaum <email": null>
2016-03-04 15:19:59 -0500

    build factories

---
+++ spec/factories.rb
@@ -0,0 +1,34 @@
+FactoryGirl.define do
+  factory :user do
+    email {Faker::Internet.email}
+    username {Faker::Hipster.word}
+    password "password"
+  end
+
+  factory :question do
+    body {Faker::Hipster.paragraph}
+    title {Faker::Hipster.sentence}
+    association :user, factory: :user
+  end
+
+  factory :answer do
+    body {Faker::Team.name}
+    association :question, factory: :question
+    association :user, factory: :user
+  end
+
+  factory :comment do
+
+    body {Faker::StarWars.quote}
+    association :user, factory: :user
+
+    factory :question_comment do
+      association :commentable, factory: :question
+    end
+
+    factory :answer_comment do
+      association :commentable, factory: :answer
+    end
+
+  end
+end

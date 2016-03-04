4f2d67986653377b9697029fe8acfe1b5a9081af
Devin Mandelbaum <email": null>
2016-03-04 16:30:46 -0500

    Add beginnings of question show path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :questions, only: [:index]
+  resources :questions, only: [:index, :show]
   resources :users, only: [:index, :create, :show]
   resource :sessions, only: [:create, :show, :test]
 

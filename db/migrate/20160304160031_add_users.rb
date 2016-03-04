ce912524da034b5eb25d1e0079282c068592d34c
Devin Mandelbaum <email": null>
2016-03-04 11:05:11 -0500

    Fill in users create table migration

--- db/migrate/20160304160031_add_users.rb
+++ db/migrate/20160304160031_add_users.rb
@@ -1,4 +1,13 @@
 class AddUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :username, null: false, index: true, unique: true
+      t.string :password_digest
+      t.string :email, null: false, unique: true
+      t.text :image_url
+      t.text :about
+
+      t.timestamps null: false
+    end
   end
 end

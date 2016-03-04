30196232e8298370cf1bbcfbd6626a9c61d5af16
Steven King <stevenking86@gmail.com>
2016-03-04 15:01:09 -0500

    Write necessary routes for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+
+  resource :sessions, only: [:create, :show]
+  get 'register' => 'users#new'
+  get 'logout' => 'sessions#destroy'
+  get 'login' => 'sessions#new'
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

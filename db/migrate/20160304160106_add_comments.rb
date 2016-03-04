3f4ec81fee67c42dc875d122c912518d9d11f138
Devin Mandelbaum <email": null>
2016-03-04 11:16:51 -0500

    Fill in comments create table migration

--- db/migrate/20160304160106_add_comments.rb
+++ db/migrate/20160304160106_add_comments.rb
@@ -1,4 +1,11 @@
 class AddComments < ActiveRecord::Migration
   def change
+    create_table :comments do |t|
+      t.text :body, null: false
+      t.references :commentable, polymorphic: true, index: true
+      t.integer :user_id, null: false
+
+      t.timestamps null: false
+    end
   end
 end

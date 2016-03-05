d72e00c3dd9df04b01faee7502699b7ef27e3438
Steven King <email": null>
2016-03-05 15:25:57 -0500

    Add ability to sort question index by most recent, trending, or highest-voted

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,18 @@ class QuestionsController < ApplicationController
     @questions = Question.all.includes(:user).order(created_at: :desc)
   end
 
+  def trending
+    @questions = Question.sort_by_trending
+  end
+
+  def most_recent
+    @questions = Question.sort_by_recent
+  end
+
+  def popular
+    @questions = Question.sort_by_votes
+  end
+
   def show
     @question = Question.includes(:user, :answers, :comments).find(params[:id])
   end

050ce1bc6242138ffdbe45b7de0b06958f735d14
Devin Mandelbaum <email": null>
2016-03-05 10:17:16 -0500

    Add ability to comment on answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :questions, only: [:index, :show, :new, :create] do
-    resources :answers, only: [:create]
+    resources :answers, only: [:create] do
+      resources :comments, only: [:create]
+    end
     resources :comments, only: [:create]
   end
 

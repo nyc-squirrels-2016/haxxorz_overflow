cb8ac06dae7f033d41b386a290c4759b16bf1e7e
Steven King <email": null>
2016-03-05 14:14:58 -0500

    Write methods to sort questions by trending and recent

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :user
-  belongs_to :question
+  belongs_to :question, touch: true
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
 
